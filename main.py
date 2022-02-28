c5846c49738976972833844fc8b19d3a51f79de8
Kiran <kiransettyks@gmail.com>
2022-02-28 23:37:20 +0200

    Set up index.html to display books saved Set up add page to add new books to library

--- main.py
+++ main.py
@@ -7,14 +7,21 @@ all_books = []
 
 @app.route('/')
 def home():
-    pass
+    return render_template("index.html", books=all_books)
 
 
-@app.route("/add")
+@app.route("/add", methods=['POST', 'GET'])
 def add():
-    pass
+    if request.method == 'GET':
+        return render_template("add.html")
+    all_books.append({'name': request.form['name'], 'author': request.form['author'], 'rating': request.form['rating']})
+    print(all_books)
+    return render_template("index.html")
+
+
 
 
 if __name__ == "__main__":
+
     app.run(debug=True)
 
