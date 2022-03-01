9a8593317146be944bc0ef077cbd0637a6e2281c
Kiran <kiransettyks@gmail.com>
2022-03-01 18:40:20 +0200

    Adding SQLAlchemy Code to collect data on books

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///new_books-collection.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), unique=True, nullable=False)
+    author = db.Column(db.String(80), unique=False, nullable=False)
+    rating = db.Column(db.Float, unique=False, nullable=False)
+
+
+db.create_all()
 
 all_books = []
-
 
 @app.route('/')
 def home():
@@ -14,9 +28,10 @@ def home():
 def add():
     if request.method == 'GET':
         return render_template("add.html")
-    all_books.append({'name': request.form['name'], 'author': request.form['author'], 'rating': request.form['rating']})
-    print(all_books)
-    return render_template("index.html")
+    new_book = Book(title=request.form['name'], author=request.form['author'], rating=request.form['rating'])
+    db.session.add(new_book)
+    db.session.commit()
+    return redirect(url_for('home'))
 
 
 
